be985b7e23d5961a6e786ebe424c85f54e9f2d7e
BanyRule <Banyrule@gmail.com>
2017-04-22 00:39:16 +0700

    Добавлено: Позиционируемая камера, сглаживание

---
+++ RayTracer/RayTracer/camera.h
@@ -0,0 +1,40 @@
+﻿// (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜ 
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+#include "ray.h"
+
+#define _USE_MATH_DEFINES
+#include <math.h> // число pi
+
+
+class Camera {
+public:
+	Camera(Vec3<double> lookFrom, Vec3<double> lookAt, Vec3<double> viewUp,double vFow, double aspect) {	// vFow угол зрения от нижнего края до верхнего
+		Vec3<double> u, v, w;	// ортогональный базис камеры						// viewUp != world up (0, 1, 0)
+		double theta = vFow * M_PI / 180;
+		double halfHeight = tan(theta / 2);
+		double halfWidth = aspect * halfHeight;
+
+		origin_ = lookFrom;
+		w = Vec3<double>::unitVector(lookFrom - lookAt);
+		u = Vec3<double>::unitVector( Vec3<double>::crossProduct(viewUp, w) );
+		v = Vec3<double>::crossProduct(w, u);	
+
+		lowerLeftCorner_ = origin_ - u * halfWidth - v * halfHeight - w;
+		horizontal_ = u * halfWidth * 2;
+		vertical_ = v * halfHeight * 2;
+	}
+
+	Ray getRay(double u, double v) {
+		return Ray(origin_, lowerLeftCorner_ + horizontal_ * u + vertical_ * v - origin_);
+	}
+
+private:
+	Vec3<double> origin_;
+	Vec3<double> lowerLeftCorner_;
+	Vec3<double> horizontal_;
+	Vec3<double> vertical_;
+};
+
+#endif	// CAMERA_H_
--- RayTracer/RayTracer/main.cc
+++ RayTracer/RayTracer/main.cc
@@ -2,11 +2,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "hitablelist.h"
+#include "camera.h"
 #include "sphere.h"
+
 
 Vec3<double> color(const Ray & r, Hitable *world) {
 	HitRecord rec;
-	if (world->hit(r, 0.0, INT_MAX, rec)) {	// луч пересек какой-то обьект
 		return Vec3<double>(rec.normal.getX() + 1, rec.normal.getY() + 1, rec.normal.getZ() + 1) * 0.5;
 	}
 	else {	// луч ушел в закат - рисуется скайбокс
@@ -19,11 +20,12 @@ Vec3<double> color(const Ray & r, Hitable *world) {
 int main() {
 	freopen("test.ppm", "w", stdout);
 
-	size_t image_with = 640;
-	size_t image_height = 320;
+	size_t imageWidth = 640;
+	size_t imageHeight = 320;
+	size_t samplesPerPixel = 16; // кол-во проб на каждый пиксель (для сглаживания)
 
 	std::cout << "P3" << std::endl;
-	std::cout << image_with << " " << image_height << "\n255" << std::endl;
+	std::cout << imageWidth << " " << imageHeight << "\n255" << std::endl;
 
 	Vec3<double> lowerLeftCorner(-2.0, -1.0, -1.0);		// левый нижний угол изображения во внутренних координатах
 	Vec3<double> horizontal(4.0, 0.0, 0.0);			// ширина изображения во внутренних координатах
@@ -31,20 +33,25 @@ int main() {
 	Vec3<double> origin(0.0, 0.0, 0.0);			// положение камеры
 
 	Hitable *list[2];
+
 	list[0] = new Sphere(Vec3<double>(0, 0, -1), 0.5);
 	list[1] = new Sphere(Vec3<double>(0, -100.5, -1), 100);
 
 	Hitable *world = new HitableList(list, 2);
 
-	for (size_t i = image_height; i > 0; --i) {
-		for (size_t j = 0; j < image_with; ++j) {
-			double u = double(j) / image_with;	// смещение по горизонтали
-			double v = double(i) / image_height;	// смещение по вертикали
-			Ray r(origin, lowerLeftCorner + horizontal * u + vertical * v);
+	for (size_t i = imageHeight; i > 0; --i) {
+		fprintf(stderr, "RENDER PROCESS: %f\n",  double(imageHeight - i) / imageHeight * 100);
+		for (size_t j = 0; j < imageWidth; ++j) {
+			Vec3<double> col(0, 0, 0);
+			for (size_t s = 0; s < samplesPerPixel; ++s) {
+				double u = double(j + (double)rand() / RAND_MAX) / imageWidth;	// смещение по горизонтали
+				double v = double(i + (double)rand() / RAND_MAX) / imageHeight;	// смещение по вертикали
+				Ray r = cam.getRay(u, v);
+				Vec3<double> p = r.pointAtParameter(2.0);
+				col += color(r, world);			// каждый пиксель собирается из нескольких проб
+			}
 
-			Vec3<double> p = r.pointAtParameter(2.0);
-			Vec3<double> col = color(r, world);
-
+			col /= double(samplesPerPixel);		// цвет проб усредняется, чтобы получить итоговый цвет
 			int ir = int(255.99 * col.getR());
 			int ig = int(255.99 * col.getG());
 			int ib = int(255.99 * col.getB());
